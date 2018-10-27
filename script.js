5a9883b76446cbf587d7a9792c1afa2b279b23c6
GW <garviswbs@gmail.com>
2018-10-27 13:37:06 -0600

    sum and date

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-    // Date
+    // SUM
+
+    var a = 5;
+    var b = 4;
+    var sum = a + b;
+    document.getElementById("sum").innerHTML = "<h3>SUM <br>var a = " + a + "<br>var b = " + b + "<br>var sum = a+b" + " = " + (a + b) + "</h3>"
+
+
+    // DATE
     var date = new Date();
-    document.body.innerHTML = "<h1>DATE <br> Today is " + date + "</h1>"
+    document.getElementById("date").innerHTML = "<h3>DATE <br> Today is " + date + "</h3>"

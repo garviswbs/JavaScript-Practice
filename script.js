6895b4aa148b3c751e387eed11c420b09b7b1386
GW <garviswbs@gmail.com>
2018-10-27 14:12:16 -0600

    3.4 conditionals

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-//OPERATORs
+// CONDITIONAL and LOGIC
+document.getElementById("conditionals").innerHTML = "<h3>CONDITIONALS<br>if Statement: if(some condition) {do something if true} else {do something if false}<br>Equality: a==b </h3>"
+
+//OPERATORS
 document.getElementById("operators").innerHTML = "<h3>OPERATORS<br>Assignment Operator: =<br>Arithmetic Operators: = + - * /<br>Shorthand Math:  += , -= , *= , /=<br>Unary Operator: a++ , a-- , ++a , --a</h3>"
 //DATA TYPES
 document.getElementById("data-types").innerHTML = "<h3>DATA TYPES<br>1. Numeric - numbers and integers<br> 2. String - strings of letters and symbols<br>3. Boolean - true or false<br>4. Null - stores the intentional absence of a value<br>5. Undefined - placeholder when a variable is not set<br>6. Symbol<br>Hint: you can define the data type using console.log(typeof variableName) "
